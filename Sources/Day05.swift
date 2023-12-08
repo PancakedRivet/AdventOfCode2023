46c053dc4ad1a0de7ffb72eac583291a4cf9a49f
Patrick <85083156+PancakedRivet@users.noreply.github.com>
2023-12-08 18:18:48 +1300

    Adding Day 5 part 1 and part 2 solution

--- Sources/Day05.swift
+++ Sources/Day05.swift
@@ -1,6 +1,12 @@
 import Algorithms
 import Foundation
 
+struct MapEntry {
+    var destinationRangeStart: Int
+    var sourceRangeStart: Int
+    var rangeLength: Int
+}
+
 struct Day05: AdventDay {
     // Save your data in a corresponding text file in the `Data` directory.
     var data: String
@@ -13,12 +19,119 @@ struct Day05: AdventDay {
         
         let DEBUG = false
         
-        var scratchCardSum: Int = 0
+        var lowestLocationNumber: Int = 0
         
-        for line in entities {
-            print(line)
-            
+        let seedList = entities[0].split(separator: " ").dropFirst(1).map { Int($0)! }
+        
+        if DEBUG {
+            print("Seeds:", seedList)
         }
-        return scratchCardSum
+        
+        var mapList: [String: [MapEntry]] = [:]
+        var mapName = ""
+        
+        var mapEntryList: [String] = []
+        
+        // Build all of the maps being used from the input lines
+        for line in entities[1...] {
+            if line.contains("map:") {
+                mapName = String(line.split(separator: " ")[0])
+                mapList[mapName] = []
+                mapEntryList.append(mapName)
+            } else {
+                let lineSplit = line.split(separator: " ")
+                let mapEntry: MapEntry = MapEntry(destinationRangeStart: Int(lineSplit[0])!, sourceRangeStart: Int(lineSplit[1])!,  rangeLength: Int(lineSplit[2])!)
+                mapList[mapName]?.append(mapEntry)
+            }
+        }
+        
+        var newSeedList: [Int] = []
+        
+        for seedValue in seedList {
+            var newSeedValue = seedValue
+            for mapName in mapEntryList {
+                let mappedSeedValue = performMap(mapSet: mapList[mapName]!, seed: newSeedValue)
+                if DEBUG {
+                    print("Map:", mapName, "Old:", newSeedValue, "New:", mappedSeedValue)
+                }
+                newSeedValue = mappedSeedValue
+            }
+            newSeedList.append(newSeedValue)
+        }
+        lowestLocationNumber = newSeedList.min()!
+        return lowestLocationNumber
+    }
+    
+    func part2() -> Any {
+        
+        let DEBUG = false
+        
+        var lowestLocationNumber: Int = 0
+        
+        let seedLine = entities[0].split(separator: " ").dropFirst(1).map { Int($0)! }
+        
+        var mapList: [String: [MapEntry]] = [:]
+        var mapName = ""
+        
+        var mapEntryList: [String] = []
+        
+        // Build all of the maps being used from the input lines
+        for line in entities[1...] {
+            if line.contains("map:") {
+                mapName = String(line.split(separator: " ")[0])
+                mapList[mapName] = []
+                mapEntryList.append(mapName)
+            } else {
+                let lineSplit = line.split(separator: " ")
+                let mapEntry: MapEntry = MapEntry(destinationRangeStart: Int(lineSplit[0])!, sourceRangeStart: Int(lineSplit[1])!,  rangeLength: Int(lineSplit[2])!)
+                mapList[mapName]?.append(mapEntry)
+            }
+        }
+        
+        for idx in stride(from: 0, to: seedLine.count, by: 2) {
+            let startingPoint = seedLine[idx]
+            let range = seedLine[idx + 1]
+            let endingPoint = startingPoint + range
+            let seedList = stride(from: startingPoint, to: endingPoint, by: 1)
+            
+            print("Searching pair:", idx / 2, "out of", seedLine.count / 2, "Numbers to check:", endingPoint - startingPoint)
+
+            for seedValue in seedList {
+                var newSeedValue = seedValue
+                for mapName in mapEntryList {
+                    let mappedSeedValue = performMap(mapSet: mapList[mapName]!, seed: newSeedValue)
+                    if DEBUG {
+                        print("Map:", mapName, "Old:", newSeedValue, "New:", mappedSeedValue)
+                    }
+                    newSeedValue = mappedSeedValue
+                }
+                
+                // Ensure that lowestLocationNumber gets set initially
+                if lowestLocationNumber <= 0 {
+                    lowestLocationNumber = newSeedValue
+                }
+                if newSeedValue < lowestLocationNumber {
+                    lowestLocationNumber = newSeedValue
+                }
+            }
+            print("Lowest location number:", lowestLocationNumber)
+        }
+        return lowestLocationNumber
+    }
+    
+    func performMap(mapSet: [MapEntry], seed: Int) -> Int {
+        var newSeedValue = seed
+        for mapLine in mapSet {
+            if !(mapLine.sourceRangeStart <= seed && seed <= mapLine.sourceRangeStart + mapLine.rangeLength) {
+                // The seed is outside the map bound so return the original value
+                continue
+            }
+            let seedValueNormalised = seed - mapLine.sourceRangeStart
+            newSeedValue = mapLine.destinationRangeStart + seedValueNormalised
+            // Seed values can only work for one map so if we find the map,
+            // we can skip trying the rest of the map lines
+            break
+        }
+        return newSeedValue
     }
 }
--- Tests/Day05.swift
+++ Tests/Day05.swift
@@ -49,6 +49,6 @@ final class Day05Tests: XCTestCase {
     
     func testPart2() throws {
         let challenge = Day05(data: testData)
-        XCTAssertEqual(String(describing: challenge.part2()), "71503")
+        XCTAssertEqual(String(describing: challenge.part2()), "46")
     }
 }
